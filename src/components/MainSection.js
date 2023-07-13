2cea48b80b62ad796f5737a1559ba8b20db08ee8
Mattias-Basle <mattias.basle@gmail.com>
2023-07-13 20:39:28 -0300

    feat: adding nav list feature

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './styles/MainSection.css';
 import '../App.css';
 import { Button } from './Button';
-import profil from '../images/profil.jpg'
+import profil from '../images/profil.jpg';
 
 function MainSection() {
   return (
     <>
       <div className='main-container' >
-        <img src={profil} alt='Profile' className='profile-image' />
+        
         <h1>Hello, I'm Mattias</h1>
         <p>This is my online take on an interactive CV</p>
         <p>Feel free to browse about the specifics,</p>
         <p>or you can click right below for an overall presentation</p>
-        <p></p>
+        <img src={profil} alt='Profile' className='profile-image' />
         <Button 
           className='btns' 
           buttonStyle='btn--outline' 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../App.css';
 import MainSection from '../MainSection';
-
 function Home() {
     return (
         <>
